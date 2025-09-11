2726442794870ea1decc16b44fb172c9fe944b8f
Subodh <s372144@studentrs.cdu.edu.au>
2025-09-12 00:03:27 +0930

    Added User.cs in models

---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyGamesProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User"; // Default role as User
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
