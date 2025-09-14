a446a1215f3adb6be430ec322a706c007a14df54
Subodh <s372144@studentrs.cdu.edu.au>
2025-09-14 18:19:56 +0930

    Added scaffold for shopping cart

---
+++ Controllers/ShoppingCartsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EasyGamesProject.Data;
+using EasyGamesProject.Models;
+
+namespace EasyGames.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShoppingCartsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ShoppingCarts
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.ShoppingCarts.Include(s => s.Stock).Include(s => s.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: ShoppingCarts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingCart = await _context.ShoppingCarts
+                .Include(s => s.Stock)
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.CartId == id);
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Create
+        public IActionResult Create()
+        {
+            ViewData["StockId"] = new SelectList(_context.Stocks, "StockId", "Category");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email");
+            return View();
+        }
+
+        // POST: ShoppingCarts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CartId,UserId,StockId,Quantity,DateAdded")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shoppingCart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StockId"] = new SelectList(_context.Stocks, "StockId", "Category", shoppingCart.StockId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", shoppingCart.UserId);
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingCart = await _context.ShoppingCarts.FindAsync(id);
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            ViewData["StockId"] = new SelectList(_context.Stocks, "StockId", "Category", shoppingCart.StockId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", shoppingCart.UserId);
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CartId,UserId,StockId,Quantity,DateAdded")] ShoppingCart shoppingCart)
+        {
+            if (id != shoppingCart.CartId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shoppingCart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShoppingCartExists(shoppingCart.CartId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StockId"] = new SelectList(_context.Stocks, "StockId", "Category", shoppingCart.StockId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", shoppingCart.UserId);
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingCart = await _context.ShoppingCarts
+                .Include(s => s.Stock)
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.CartId == id);
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shoppingCart = await _context.ShoppingCarts.FindAsync(id);
+            if (shoppingCart != null)
+            {
+                _context.ShoppingCarts.Remove(shoppingCart);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShoppingCartExists(int id)
+        {
+            return _context.ShoppingCarts.Any(e => e.CartId == id);
+        }
+    }
+}
